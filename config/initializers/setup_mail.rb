8a3e2cb1583567d1ab30cec5a143bd214109cd65
robert lu <dao888@gmail.com>
2013-03-18 21:20:32 -0700

    tst'

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,8 +5,9 @@
 
 ActionMailer::Base.smtp_settings = {
   :address  => "mail.usroc.com",
-  :port  => 25,
+  :port  => 995,
   :user_name  => "accounting",
   :password  => "dollar8888",
-  :authentication  => :login
+  :authentication  => :login,
+  :enable_starttls_auto => true
 }
