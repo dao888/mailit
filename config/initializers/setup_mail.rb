0a55eabfae7b7a5befbb0ea928d26f5262cf2c97
robert lu <dao888@gmail.com>
2013-03-18 21:30:40 -0700

    test

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -6,7 +6,7 @@
 ActionMailer::Base.smtp_settings = {
   :address  => "mail.usroc.com",
   :port  => 995,
-  :user_name  => "accounting",
+  :user_name  => "accounting/usroc.com",
   :password  => "dollar8888",
   :authentication  => :login
 }
