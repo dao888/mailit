25a834c643777802235a64090e3a0a0b769a1885
robert lu <dao888@gmail.com>
2013-03-20 12:20:44 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Mailit::Application.configure do
 	
 	#heroku recommendation
-	config.action_mailer.delivery_method = :smtp
+	#config.action_mailer.delivery_method = :smtp
 	
 	
   # Settings specified here will take precedence over those in config/application.rb
