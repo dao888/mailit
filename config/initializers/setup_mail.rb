27d17b57dc4402555782ca008eb0c751529c24ee
robert lu <dao888@gmail.com>
2013-03-20 12:27:40 -0700

    test

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -6,8 +6,8 @@
 ActionMailer::Base.smtp_settings = {
   :address  => "mail.sendgrid.net",
   :port  => 587,
-  :user_name  => ENV['app13639788@heroku.com'],
-  :password  => ENV['zurw99jb'],
+  :user_name  => ENV['SENDGRID_USERNAME'],
+  :password  => ENV['SENDGRID_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true,
   :domain         => 'heroku.com'
