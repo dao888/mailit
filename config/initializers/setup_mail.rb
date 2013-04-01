4e979f919d2f7878c5aa23068bb97e53f61fc64b
robert lu <dao888@gmail.com>
2013-04-01 12:51:14 -0700

    add mail to gemfile

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,13 @@
  
  
  
-  ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "dao888@gmail.com",
-  :password             => "dao4239jupiter",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+
+
+ActionMailer::Base.smtp_settings = {
+  :address  => "mail.usroc.com",
+  :port  => 25,
+  :user_name  => "accounting/usroc.com",
+  :password  => "dollar8888",
+  :authentication  => :login
 }
+
